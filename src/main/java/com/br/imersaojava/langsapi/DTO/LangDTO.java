30c40932e550ea4c3e0f1f276bb2eef161a54447
Lukas Veiga <lukas.veiga@hotmail.com>
2023-04-10 09:20:57 -0300

    dto class to record

--- src/main/java/com/br/imersaojava/langsapi/DTO/LangDTO.java
+++ src/main/java/com/br/imersaojava/langsapi/DTO/LangDTO.java
@@ -2,20 +2,11 @@ package com.br.imersaojava.langsapi.DTO;
 
 import com.br.imersaojava.langsapi.model.Lang;
 import jakarta.validation.constraints.NotEmpty;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-public class LangDTO {
-
-    @NotEmpty(message = "The name is required.")
-    private String title;
-    @NotEmpty(message = "The image url is required.")
-    private String image;
-
+public record LangDTO(
+        @NotEmpty(message = "The name is required") String title,
+        @NotEmpty(message = "The image url is required") String image
+) {
     public Lang transformToObject() {
         return new Lang(this.title, this.image);
     }
--- src/main/java/com/br/imersaojava/langsapi/model/Lang.java
+++ src/main/java/com/br/imersaojava/langsapi/model/Lang.java
@@ -1,8 +1,6 @@
 package com.br.imersaojava.langsapi.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
