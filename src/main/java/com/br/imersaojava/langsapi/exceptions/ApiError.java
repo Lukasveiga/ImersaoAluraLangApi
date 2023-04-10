5e8c03bdda49dd013c269da37ead824ef17136ac
Lukas Veiga <lukas.veiga@hotmail.com>
2023-04-10 09:54:18 -0300

    update exception handler

---
+++ src/main/java/com/br/imersaojava/langsapi/exceptions/ApiError.java
@@ -0,0 +1,11 @@
+package com.br.imersaojava.langsapi.exceptions;
+
+import java.time.LocalDateTime;
+
+public record ApiError(
+        String path,
+        String message,
+        int statusCode,
+        LocalDateTime localDateTime
+) {
+}
--- src/main/java/com/br/imersaojava/langsapi/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/br/imersaojava/langsapi/exceptions/ResourceExceptionHandler.java
@@ -1,22 +1,39 @@
 package com.br.imersaojava.langsapi.exceptions;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.time.LocalDateTime;
+
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
     @ExceptionHandler(LangNotFoundException.class)
-    public ResponseEntity<?> langNotFound(LangNotFoundException e) {
-        e.printStackTrace();
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    public ResponseEntity<?> langNotFound(LangNotFoundException e, HttpServletRequest request) {
+
+        ApiError apiError = new ApiError(
+                request.getRequestURI(),
+                e.getMessage(),
+                HttpStatus.NOT_FOUND.value(),
+                LocalDateTime.now()
+        );
+
+        return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(LangAlreadyExistsException.class)
-    public ResponseEntity<?> langAlreadyExists(LangAlreadyExistsException e) {
-        e.printStackTrace();
-        return ResponseEntity.status(HttpStatus.OK).body(e.getMessage());
+    public ResponseEntity<?> langAlreadyExists(LangAlreadyExistsException e, HttpServletRequest request) {
+
+        ApiError apiError = new ApiError(
+                request.getRequestURI(),
+                e.getMessage(),
+                HttpStatus.OK.value(),
+                LocalDateTime.now()
+        );
+
+        return new ResponseEntity<>(apiError, HttpStatus.OK);
     }
 }
